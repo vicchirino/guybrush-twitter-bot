f276c8d2bd375df604e22fd09999d41f44177c2d
Victor Chirino <vicchirino@gmail.com>
2022-07-01 12:06:48 -0300

    Move bots into bots folder. Add Monkey Island II frames

--- utils/convert_video_to_frames.py
+++ utils/convert_video_to_frames.py
@@ -1,15 +1,15 @@
 import cv2
 
-framesPath = "/frames/monkey-island-I/"
-videoPath = "/videos/monkey-island-I/TheSecretOfMonkeyIslandFull.mp4"
+framesPath = "/Users/victorchirino/Projects/guybrush-twitter-bot/frames/monkey-island-III/"
+videoPath = "/Users/victorchirino/Projects/guybrush-twitter-bot/videos/monkey-island-III/monkey-island-III.mp4"
 
 def extractImages():
     vidcap = cv2.VideoCapture(videoPath)
     success,image = vidcap.read()
-    count = 2
+    count = 1
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*5000))
-        cv2.imwrite(framesPath + "frame%d.jpg" % count, image)
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*5000)) ## Screenshots every 5 seconds
+        cv2.imwrite(framesPath + "frame-%d.jpg" % count, image)
         success,image = vidcap.read()
         print("Read a new frame: ", success)
         count += 1
